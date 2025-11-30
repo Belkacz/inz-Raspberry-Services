02dd80c199f8f5b2eff3a98e8609449ea795586e
Belkacz <belka.lukasz.job@gmail.com>
2025-11-30 15:27:48 +0100

    [stage#2] added stream fps controol to cam_service.c

--- cam_service/cam_service.c
+++ cam_service/cam_service.c
@@ -4,27 +4,41 @@
 #include <libuvc/libuvc.h>
 #include <libwebsockets.h>
 #include <stdbool.h>
-#include "../common.h"
+#include <time.h>
+#include "common.h"
 
 #define PORT 2138
 #define MAX_FRAME_SIZE (4 * 1024 * 1024)
 #define FPS 30
+#define STREAM_FPS 20  // Docelowy FPS dla WebSocket
 
 typedef struct {
     unsigned char frameBuffer[MAX_FRAME_SIZE];
     size_t frameSize;
     volatile int hasNewFrame;
     volatile bool connectionEstablished;
+    struct timespec lastSentTime;
 } AppState;
+
+// Pomocnicza funkcja do liczenia różnicy czasu w ms
+static long long timespec_diff_ms(struct timespec *start, struct timespec *end)
+{
+    return (end->tv_sec - start->tv_sec) * 1000LL + 
+           (end->tv_nsec - start->tv_nsec) / 1000000LL;
+}
 
 static void callbackUVC(uvc_frame_t *frame, void *ptr)
 {
     AppState *state = (AppState*)ptr;
-    if (frame->data_bytes > MAX_FRAME_SIZE)
+    
+    // Podstawowe sprawdzenia
+    if (!frame || frame->data_bytes == 0 || frame->data_bytes > MAX_FRAME_SIZE)
     {
         return;
     }
     
+    // PROSTY MODEL: Po prostu nadpisz bufor najnowszą ramką
+    // Jeśli poprzednia nie została wysłana - trudno, nadpisujemy
     memcpy(state->frameBuffer, frame->data, frame->data_bytes);
     state->frameSize = frame->data_bytes;
     state->hasNewFrame = 1;
@@ -33,39 +47,71 @@ static void callbackUVC(uvc_frame_t *frame, void *ptr)
 static int callbackWs(struct lws *wsi, enum lws_callback_reasons reason,
                       void *user, void *in, size_t len)
 {
-    in=in;   // Wycisz ostrzeżenie
-    len=len;
-    user=user;
+    (void)in;
+    (void)len;
+    (void)user;
+    
     AppState *state = (AppState *)lws_context_user(lws_get_context(wsi));
+    if (!state) {
+        return -1;
+    }
+    
     switch (reason)
     {
     case LWS_CALLBACK_ESTABLISHED:
-        // printf("✓ Klient połączony\n");
+        fprintf(stderr, "[WS] Klient połączony\n");
         lwsl_user("Nowe połączenie WebSocket\n");
         state->connectionEstablished = true;
+        
+        // Zresetuj czas ostatniego wysłania
+        clock_gettime(CLOCK_MONOTONIC, &state->lastSentTime);
+        
         lws_callback_on_writable(wsi);
         break;
         
     case LWS_CALLBACK_SERVER_WRITEABLE:
-        if (state->hasNewFrame && state->frameSize > 0)
-        {
-            unsigned char *buf = malloc(LWS_PRE + state->frameSize);
-            if (buf)
-            {
-                memcpy(buf + LWS_PRE, state->frameBuffer, state->frameSize);
-                lws_write(wsi, buf + LWS_PRE, state->frameSize, LWS_WRITE_BINARY);
-                free(buf);
-                state->hasNewFrame = 0;
-            }
+    {
+        if (!state->hasNewFrame || state->frameSize == 0) {
+            // Brak nowej ramki - poproś o kolejne wywołanie
+            lws_callback_on_writable(wsi);
+            break;
         }
+        
+        // Sprawdź czy minął wystarczający czas od ostatniego wysłania
+        struct timespec timeNow;
+        clock_gettime(CLOCK_MONOTONIC, &timeNow);
+        long long elapsedTime = timespec_diff_ms(&state->lastSentTime, &timeNow);
+
+        // Oblicz minimalny interwał (w ms) między ramkami
+        long long fpsInterval = 1000 / STREAM_FPS;
+
+        if (elapsedTime < fpsInterval) {
+            // Za wcześnie - pomiń tę ramkę
+            // Poproś o kolejne wywołanie w następnym cyklu
+            lws_callback_on_writable(wsi);
+            break;
+        }
+        // Wysyłanie klatki
+        unsigned char *buf = malloc(LWS_PRE + state->frameSize);
+        if (buf)
+        {
+            memcpy(buf + LWS_PRE, state->frameBuffer, state->frameSize);
+            lws_write(wsi, buf + LWS_PRE, state->frameSize, LWS_WRITE_BINARY);
+            free(buf);
+            state->hasNewFrame = 0;
+            state->lastSentTime = timeNow;
+        }
+        // Poproś o kolejne wywołanie
         lws_callback_on_writable(wsi);
         break;
+    }
         
     case LWS_CALLBACK_CLOSED:
-        // printf("Klient rozłączony\n");
+        fprintf(stderr, "[WS] Klient rozłączony\n");
         lwsl_user("Połączenie WebSocket zakończone\n");
         state->connectionEstablished = false;
         break;
+        
     default:
         break;
     }
@@ -78,6 +124,7 @@ int main(void)
     signal(SIGTERM, handleSignal);
     signal(SIGSEGV, handleSignal);
     signal(SIGABRT, handleSignal);
+
     FILE *logFile = fopen("/var/log/camService.log", "a");
     setvbuf(logFile, NULL, _IOLBF, 0);
     stderr = logFile;
@@ -85,7 +132,8 @@ int main(void)
     AppState state = {
         .frameSize = 0,
         .hasNewFrame = 0,
-        .connectionEstablished = false
+        .connectionEstablished = false,
+        .lastSentTime = {0, 0}
     };
 
     uvc_context_t *camContext;
@@ -131,6 +179,7 @@ int main(void)
         return 1;
     }
 
+    // --- WebSocket ---
     struct lws_protocols protocols[] =
     {   
         // const char *name, lws_callback_function *callback , size_t per_session_data_size, size_t rx_buffer_size, unsigned int id , void *user, size_t tx_packet_size
@@ -138,7 +187,6 @@ int main(void)
         { NULL, NULL, 0, 0, 0, NULL, 0 }
     };
 
-    // WebSocket serwer
     struct lws_context_creation_info info;
     memset(&info, 0, sizeof info);
     info.port = PORT;
@@ -149,15 +197,17 @@ int main(void)
     if (!lwsContext)
     {
         fprintf(stderr, "Błąd: nie udało się utworzyć kontekstu WebSocket\n");
+        uvc_close(devHandler);
+        uvc_unref_device(device);
+        uvc_exit(camContext);
         return 1;
     }
 
     printf("Serwer WebSocket działa na ws://<IP>:%d\n", PORT);
     while (!stopRequested)
     {
-        // Start isStreaming gdy klient się połączy
-        if (state.connectionEstablished && !isStreaming)
-        {
+        // Start streaming
+        if (state.connectionEstablished && !isStreaming) {
             // uvc_device_handle_t *devh, uvc_stream_ctrl_t *ctrl, uvc_frame_callback_t *cb,void *user_ptr, uint8_t flags
             res = uvc_start_streaming(devHandler, &streamCtrl, callbackUVC, &state, 0);
             if (res < 0)
@@ -171,17 +221,17 @@ int main(void)
         }
         
         // Stop isStreaming gdy klient się rozłączy
-        if (!state.connectionEstablished && isStreaming)
-        {
+        if (!state.connectionEstablished && isStreaming) {
             printf("Rozłączono – zatrzymuję stream\n");
             uvc_stop_streaming(devHandler);
             isStreaming = false;
         }
         
+        // Obsługa WebSocket
         lws_service(lwsContext, 10);
     }
 
-    //  Cleanup
+    // Cleanup
     if (isStreaming)
     {
         uvc_stop_streaming(devHandler);
@@ -191,7 +241,7 @@ int main(void)
     uvc_exit(camContext);
     lws_context_destroy(lwsContext);
 
-    if (logFile)
+        if (logFile)
     {
         fclose(logFile);
     }
